07014ad0614fcffff338925d4ef5cf49eac6246f
BLAZETEAMBOT <34600028+blazeteambot@users.noreply.github.com>
2017-12-19 17:54:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ if($message['type']=='sticker')
 }
 else
 $pesan=str_replace(" ", "%20", $pesan_datang);
-$key = '9f256a21-5d2f-4ae2-874a-e500a50bb04c'; //API SimSimi
+$key = '18b4931c-bde5-49ef-8bac-194ce1f6e4a3'; //API SimSimi
 $url = 'http://sandbox.api.simsimi.com/request.p?key='.$key.'&lc=id&ft=1.0&text='.$pesan;
 $json_data = file_get_contents($url);
 $url=json_decode($json_data,1);
